e3a80ce4293192189ecb907d74f309c9ceb82265
Sivan Patel <sivan.patel@googlemail.com>
2015-07-13 16:55:04 -0400

    Added shuffle method

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,30 @@
-def shuffle arr
-  # your code here
+    def shuffle arr
+    #empty array for shuffled items to go into
+    shuffled = []
+
+    #take random item from original array and put it into shuffled
+    #keep the rest where they are
+    #then create new array with arr so there isnt an empty slot in arr
+    while arr.length>0
+
+    rand_number=rand(arr.length)
+    current_number=rand(arr.length)
+    new_arr=[]
+
+        arr.each do |item|
+            if rand_number==current_number
+                shuffled.push item
+            else
+                new_arr.push item
+            end
+            puts arr.inspect
+            arr=new_arr
+            current_number=rand(arr.length)
+        end
+        
+
+    end
+    shuffled
+
 end
+
