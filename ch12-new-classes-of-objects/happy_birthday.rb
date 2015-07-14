334e8f70c0c9baa1fd39aae8c84bb7c30f581b01
Sivan Patel <sivan.patel@googlemail.com>
2015-07-14 13:56:52 -0400

    Birthday Spanks

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,16 @@
-# your code here
+puts 'What year were you born in?'
+year=gets.chomp
+puts 'What month were you born in?'
+month=gets.chomp
+puts 'What day were you born on?'
+day=gets.chomp
+
+bday_time = Time.local(year, month, day)
+
+time_diff = Time.new - bday_time
+
+years = time_diff/(60*60*24*365)
+
+spanks = years.to_i
+
+puts 'SPANK! '*spanks
