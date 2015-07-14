1273e0ac7a0510db790c75ecad85d6f459d25e74
Sivan Patel <sivan.patel@googlemail.com>
2015-07-14 14:58:08 -0400

    not working yet, come back to this

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,35 @@
 def roman_to_integer roman
-  # your code here
+  numbers= {'i' => 1,
+  			'v' => 5,
+  			'x' => 10,
+  			'l' => 50,
+  			'c' => 100,
+  			'd' => 500,
+  			'm' => 1000,
+  			}
+
+total = 0
+so_far = 0
+
+
+starting = roman.length -1
+
+while starting >= 0
+      starting = starting - 1
+    lower = roman.downcase
+    value = numbers[lower]
+
+    if value.to_i < so_far.to_i
+      val *= -1
+    else
+      so_far=value
+    end
+
+    total = total + value.to_i
 end
+
+total
+
+end
+
+puts(roman_to_integer('mcmx'))
